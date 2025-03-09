622e5ef336f39ad667006b72cb4072eaa1b7f9d0
testudoDaemon <tade.castillo.03@gmail.com>
2025-03-09 16:44:26 -0400

    Postgresql

---
+++ postgresql/scripts/01_tablas.sql
@@ -0,0 +1,7 @@
+CREATE TABLE users (
+ID SERIAL PRIMARY KEY,
+name VARCHAR(30),
+email VARCHAR(30));
+
+INSERT INTO users VALUES(1,'Memoreno','moreno@gmail.com');
+INSERT INTO users VALUES(2,'YO','yoyo@gmail.com');
